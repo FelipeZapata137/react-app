3d1015149ce27c1bd3df4812c107b0deb832bf16
FelipeZapata137 <felipe.zapata@epn.edu.ec>
2025-06-19 22:10:09 -0500

    Mejorando la parte de informacion personal

--- mi-react-app/src/App.jsx
+++ mi-react-app/src/App.jsx
@@ -6,11 +6,17 @@ function App() {
   return (
     <div className="cv-principal-contenedor">
       <div className="columna-izquierda">
-        {/* Aquí iría tu foto, información de contacto, aptitudes, etc. */}
+        <section>
+          <img src="/src/assets/fotoPerfil.jpg" alt="Foto de Perfil" width="250" height="300" className='estilo-foto'></img>
         <p>Contenido de la Columna Izquierda</p>
+        </section>
+        <div className = 'info-personal'>
         <p><strong>Email:</strong> felipe.zapata@epn.edu.ec</p>
         <p><strong>Teléfono:</strong> +593 987 269 974</p>
         <div className="contenedor-foto"></div>
+        <Habilidades />
+        </div>
+        
       </div>
 
       {/* Esta será la columna derecha (donde va el resumen, experiencia, etc.) */}
@@ -18,11 +24,17 @@ function App() {
         {/* Aquí iría tu resumen profesional, historial laboral, educación, etc. */}
       <h1>Felipe Zapata</h1>
 
-
-
       <section className="seccion-resumen">
         <h2>Resumen Profesional</h2>
-        <p>Estudiante apasionado por la tecnología con ganas de aprender y aplicar mis conocimientos en el desarrollo web. Buscando oportunidades para crecer en el campo de la programación.</p>
+        <p>Estudiante de la Escuela Politécnica Nacional con dos años de sólida formación 
+          autodidacta en desarrollo de software, aspirando a convertirme en un desarrollador 
+          Full Stack altamente capacitado. Poseo un dominio en Java, C++, Python, JavaScript, 
+          HTML5, CSS3, Git, GitHub y Figma, complementado con un entendimiento de metodologías 
+          ágiles como Scrum. Mi fortaleza radica en mi pensamiento crítico y habilidad para 
+          resolver problemas lógicos de manera óptima, identificando siempre las soluciones 
+          más eficientes. Como líder nato, mi enfoque centrado y gran adaptabilidad me permiten
+           integrarme y aportar valor rápidamente a equipos innovadores. Estoy listo para aplicar
+            mis conocimientos y mi compromiso con la excelencia para impulsar el éxito de su empresa.</p>
       </section>
 
       <section className="seccion-educacion">
@@ -31,8 +43,6 @@ function App() {
         <p><strong>Carrera:</strong> Tecnología Superior en Desarrollo de Software</p>
         <p><strong>Años:</strong> 2023 - 2025</p>
       </section>
-
-      <Habilidades />
 
       {/* Puedes añadir más secciones como Experiencia Laboral, Proyectos, etc. */}
 
--- mi-react-app/src/Componentes/habilidades.jsx
+++ mi-react-app/src/Componentes/habilidades.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './habilidades.css'
 
 function Habilidades() {
   const listaHabilidades = [
